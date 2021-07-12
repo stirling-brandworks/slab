ae6d14a74aae6c47bc10b1f42aff66425f04b59a
Brian John Hanna <bjhanna728@gmail.com>
2021-07-12 17:06:16 -0400

    Override composer install script to exclude preset option

---
+++ app/Installer/ComposerScript.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Installer;
+
+use Roots\Sage\Installer\ComposerScript as RootsComposerScript;
+use Composer\Script\Event;
+use Symfony\Component\Process\Process;
+
+class ComposerScript extends RootsComposerScript
+{
+    /**
+     * Override the Sage hook to exclude the preset override
+     *
+     * @see Roots\Sage\Installer\ComposerScript::postCreateProject
+     *
+     * @param Event $event
+     * @return void
+     */
+    public static function postCreateProject(Event $event)
+    {
+        $sage = escapeshellarg(dirname(__DIR__) . '/bin/sage');
+        (new static($event))
+            ->validate()
+            ->run(new Process(sprintf('php %s %s', $sage, 'meta')))
+            ->run(new Process(sprintf('php %s %s', $sage, 'config')));
+    }
+}
