0600ffe514edcaa162227b4d4717b2ed3df7f8e6
Brian Hanna <bjhanna728@gmail.com>
2021-07-30 14:14:35 -0400

    Update layout of kitchen sink page

--- resources/views/template-kitchen-sink.blade.php
+++ resources/views/template-kitchen-sink.blade.php
@@ -13,12 +13,14 @@
 
 <hr />
 
-<h2>Cards</h2>
+<h2 class="mb-3">Cards</h2>
 <h3>Single Card</h3>
-@include('components.card', $single_card['card'])
+<div class="mb-5" style="width: 32rem;">
+  @include('components.card', $single_card['card'])
+</div>
 
 <h3>Multi Cards</h3>
-<div class="row">
+<div class="row mb-5">
   @foreach ($multi_card as $card)
   <div class="col">
     @include('components.card', $card['card'])
@@ -28,24 +30,28 @@
 
 <hr />
 
-<h2>Sliders & Carousels</h2>
-<h3>Slider</h3>
-@if ($slider['slides'])
-@include('components.slider', $slider)
-@endif
+<h2 class="mb-3">Sliders & Carousels</h2>
+<h3 class="mb-3">Slider</h3>
+<div class="mb-5">
+  @if ($slider['slides'])
+  @include('components.slider', $slider)
+  @endif
+</div>
 
-<h3>Carousel</h3>
+<h3 class="mb-3">Carousel</h3>
 TBD
 
 <hr />
 
-<h2>Accordions & Tabs</h2>
-<h3>Accordion</h3>
-@if ($accordion_items)
-@include('components.accordion', ['items' => $accordion_items])
-@endif
+<h2 class="mb-3">Accordions & Tabs</h2>
+<h3 class="mb-3">Accordion</h3>
+<div class="mb-5">
+  @if ($accordion_items)
+  @include('components.accordion', ['items' => $accordion_items])
+  @endif
+</div>
 
-<h3>Tabs</h3>
+<h3 class="mb-3">Tabs</h3>
 @if ($tabs)
 @include('components.tabs', $tabs)
 @endif
