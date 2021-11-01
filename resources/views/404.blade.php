7ff3869d26e7794701952770b5ad18bfc19944e0
sarahst <sarah@stirlingbrandworks.com>
2021-11-01 12:46:23 -0400

    Updated the layout to full-width for search and 404 page

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,12 +1,15 @@
-@extends('layouts.default')
+@extends('layouts.full-width')
 
 @section('content')
 @include('partials.page-header')
-
-@if (!have_posts())
-<div class="alert alert-warning">
-  {{ __('Sorry, but the page you were trying to view does not exist.', 'slab') }}
+<div class="pt-3">
+	<div class="container">
+		@if (!have_posts())
+		<div class="alert alert-warning">
+		  {{ __('Sorry, but the page you were trying to view does not exist.', 'slab') }}
+		</div>
+		{!! get_search_form(false) !!}
+		@endif
+	</div>
 </div>
-{!! get_search_form(false) !!}
-@endif
 @endsection
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,18 +1,21 @@
-@extends('layouts.default')
+@extends('layouts.full-width')
 
 @section('content')
 @include('partials.page-header')
+<div class="pt-3">
+	<div class="container">
+		@if (!have_posts())
+		<div class="alert alert-warning">
+		  {{ __('Sorry, no results were found.', 'slab') }}
+		</div>
+		{!! get_search_form(false) !!}
+		@endif
 
-@if (!have_posts())
-<div class="alert alert-warning">
-  {{ __('Sorry, no results were found.', 'slab') }}
+		@while(have_posts()) @php the_post() @endphp
+		@include('partials.content-search')
+		@endwhile
+
+		{!! get_the_posts_navigation() !!}
+	</div>
 </div>
-{!! get_search_form(false) !!}
-@endif
-
-@while(have_posts()) @php the_post() @endphp
-@include('partials.content-search')
-@endwhile
-
-{!! get_the_posts_navigation() !!}
 @endsection
