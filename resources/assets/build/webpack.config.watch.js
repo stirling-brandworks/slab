afb23a37f0b64f31cc9c5213183be6acfc94d842
Dimitri Pierre-Louis <dimitri.dev@icloud.com>
2021-11-02 16:33:04 -0400

    HotModuleReplacementPlugin: add quiet parameter for FriendlyErrorsWebpackPlugin

--- resources/assets/build/webpack.config.watch.js
+++ resources/assets/build/webpack.config.watch.js
@@ -5,31 +5,44 @@ const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 const config = require('./config');
 
 const target = process.env.DEVURL || config.devUrl;
-
-/**
- * We do this to enable injection over SSL.
- */
-if (url.parse(target).protocol === 'https:') {
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
-
-  config.proxyUrl = config.proxyUrl.replace('http:', 'https:');
-}
+const proxyUrlObj = url.parse(config.proxyUrl);
 
 module.exports = {
-  output: {
-    pathinfo: true,
-    publicPath: config.proxyUrl + config.publicPath,
-  },
-  devtool: 'cheap-module-source-map',
-  stats: false,
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    output: {
+        pathinfo: true,
+        publicPath: config.proxyUrl + config.publicPath,
+    },
+    devtool: 'cheap-module-source-map',
+    stats: false,
+    plugins: [
     new BrowserSyncPlugin({
-      target,
-      open: config.open,
-      proxy: config.proxyUrl,
-      watch: config.watch,
-      delay: 500,
+        host:  proxyUrlObj.hostname,
+        port:  proxyUrlObj.port,
+
+        proxy: target,
+        https: (url.parse(target).protocol === 'https:'),
+
+        files: [
+        '**/*.php',
+        '**/*.css',
+        {
+            match: '**/*.js',
+            options:{
+                ignored: 'dist/**/*.js',
+            },
+        },
+        ],
+
+        watch: config.watch,
+        open:  config.open,
+        reloadDelay: 500,
+    },{
+        reload: false,
+        injectChanges: false,
+    }),
+
+    new webpack.HotModuleReplacementPlugin({
+        quiet: true, // for Friendly-errors-webpack-plugin
     }),
   ],
 };
