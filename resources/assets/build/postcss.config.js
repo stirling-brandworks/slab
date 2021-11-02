90896921bdd18b27d7efbebee2718cc72248cd45
Dimitri Pierre-Louis <dimitri.dev@icloud.com>
2021-11-02 15:38:46 -0400

    autoprefixer downgraded, later version require ^PostCSS 8

--- resources/assets/build/postcss.config.js
+++ resources/assets/build/postcss.config.js
@@ -9,6 +9,7 @@ module.exports = (api) => {
   return {
     parser: api.options.ctx.enabled.optimize ? 'postcss-safe-parser' : undefined,
     plugins: {
+      autoprefixer: true,
       cssnano: api.options.ctx.enabled.optimize ? cssnanoConfig : false,
     },
   };
