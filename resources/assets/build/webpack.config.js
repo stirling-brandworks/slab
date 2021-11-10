1f537ffcc9e44aefc921c4daee8ed68e6226d666
Dimitri Pierre-Louis <dimitri.michel@hey.com>
2021-11-10 10:15:33 -0500

    Webpack 5 Update (#30)
    * package & config updates
    
    * Added Support ^Node 12
    
    * Node Version now >=12.0.0
    
    * DartSass(JS) removed in favor or Node-Sass
    
    * moved appropriate packaged to devDepencency
    
    * removed postcss & added autoprefixer@latest
    
    * autoprefixer downgraded, later version require ^PostCSS 8
    
    * removed cache loader (depreacted) in favor of webpack 5 cache & added webpack 5 compatible for of FriendlyErrorsWebpackPlugin
    
    * HotModuleReplacementPlugin: add quiet parameter for FriendlyErrorsWebpackPlugin
    
    * Added PostCSS 8 for Webpack 5 Compatibility (https://github.com/postcss/postcss/wiki/PostCSS-8-for-end-users)
    
    * Fix deprecation warning for hash
    
    * Fixed Issue where urls weren't resolving correctly, moved packages to appropriate dependencies
    
    * removes unused comment
    
    * PostCSS config desctructed
    
    * destructuring && sourceMaps now working w/ value from config
    
    * Update node version in .nvmrc
    
    * Remove duplicate friendly-errors-webpack-plugin dep
    
    * Revert change to url-loader version contraint
    
    * Simplify webpack config stats object
    
    * Update yarn.lock
    
    * Remove redunant webpack-dev-middleware
    
    https://stackoverflow.com/a/42354413/2611917
    
    * Update path for @popperjs
    
    * Fix indentation in config files
    
    Co-authored-by: Brian Hanna <bjhanna728@gmail.com>

--- resources/assets/build/webpack.config.js
+++ resources/assets/build/webpack.config.js
@@ -1,49 +1,43 @@
 'use strict'; // eslint-disable-line
 
+const path = require('path');
 const webpack = require('webpack');
-const merge = require('webpack-merge');
-const CleanPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const { merge, mergeWithCustomize, customizeArray } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
-const CopyGlobsPlugin = require('copy-globs-webpack-plugin');
-const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('@soda/friendly-errors-webpack-plugin'); // maintained fork for webpack 5
 
 const desire = require('./util/desire');
 const config = require('./config');
 
-const assetsFilenames = (config.enabled.cacheBusting) ? config.cacheBusting : '[name]';
+const assetsFilenames = config.enabled.cacheBusting
+  ? config.cacheBusting
+  : '[name]';
 
 let webpackConfig = {
   context: config.paths.assets,
   entry: config.entry,
-  devtool: (config.enabled.sourceMaps ? '#source-map' : undefined),
+  devtool: config.enabled.sourceMaps ? 'source-map' : undefined,
   output: {
     path: config.paths.dist,
     publicPath: config.publicPath,
     filename: `scripts/${assetsFilenames}.js`,
   },
-  stats: {
-    hash: false,
-    version: false,
-    timings: false,
-    children: false,
-    errors: false,
-    errorDetails: false,
-    warnings: false,
-    chunks: false,
-    modules: false,
-    reasons: false,
-    source: false,
-    publicPath: false,
+  /**
+   * Suppress stats/errors in favor of the friendly-errors-webpack-plugin display
+   * https://github.com/sodatea/friendly-errors-webpack-plugin#turn-off-errors
+   * https://webpack.js.org/configuration/stats/
+   */
+  stats: false,
+  performance: {
+    maxEntrypointSize: 1024000,
+    maxAssetSize: 1024000,
   },
   module: {
     rules: [
-      {
-        enforce: 'pre',
-        test: /\.js$/,
-        include: config.paths.assets,
-        use: 'eslint',
-      },
       {
         enforce: 'pre',
         test: /\.(js|s?[ca]ss)$/,
@@ -54,110 +48,97 @@ let webpackConfig = {
         test: /\.js$/,
         exclude: [/node_modules(?![/|\\](bootstrap|foundation-sites))/],
         use: [
-          { loader: 'cache' },
-          { loader: 'buble', options: { objectAssign: 'Object.assign' } },
+          {
+            loader: 'buble-loader',
+            options: { objectAssign: 'Object.assign' },
+          },
         ],
       },
       {
         test: /\.css$/,
         include: config.paths.assets,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style',
-          use: [
-            { loader: 'cache' },
-            { loader: 'css', options: { sourceMap: config.enabled.sourceMaps } },
-            {
-              loader: 'postcss', options: {
-                postcssOptions: { path: __dirname, ctx: config },
-                sourceMap: config.enabled.sourceMaps,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: config.enabled.sourceMaps },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                config: path.join(__dirname, 'postcss.config.js'),
+                ctx: config,
               },
+              sourceMap: config.enabled.sourceMaps,
             },
-          ],
-        }),
+          },
+        ],
       },
       {
         test: /\.scss$/,
-        include: config.paths.assets,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style',
-          use: [
-            { loader: 'cache' },
-            { loader: 'css', options: { sourceMap: config.enabled.sourceMaps } },
-            {
-              loader: 'postcss', options: {
-                postcssOptions: { path: __dirname, ctx: config },
-                sourceMap: config.enabled.sourceMaps,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: config.enabled.sourceMaps },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                config: path.join(__dirname, 'postcss.config.js'),
+                ctx: config,
               },
+              sourceMap: config.enabled.sourceMaps,
             },
-            { loader: 'resolve-url', options: { sourceMap: config.enabled.sourceMaps } },
-            {
-              loader: 'sass', options: {
-                sourceMap: config.enabled.sourceMaps,
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
                 sourceComments: true,
               },
+              sourceMap: config.enabled.sourceMaps,
             },
-          ],
-        }),
-      },
-      {
-        test: /\.(ttf|otf|eot|woff2?|png|jpe?g|gif|svg|ico)$/,
-        include: config.paths.assets,
-        loader: 'url',
-        options: {
-          limit: 4096,
-          name: `[path]${assetsFilenames}.[ext]`,
-        },
-      },
-      {
-        test: /\.(ttf|otf|eot|woff2?|png|jpe?g|gif|svg|ico)$/,
-        include: /node_modules/,
-        loader: 'url',
-        options: {
-          limit: 4096,
-          outputPath: 'vendor/',
-          name: `${config.cacheBusting}.[ext]`,
-        },
+          },
+          {
+            loader: 'resolve-url-loader',
+          },
+        ],
       },
     ],
   },
   resolve: {
-    modules: [
-      config.paths.assets,
-      'node_modules',
-    ],
+    modules: [config.paths.assets, 'node_modules'],
     enforceExtension: false,
-  },
-  resolveLoader: {
-    moduleExtensions: ['-loader'],
   },
   externals: {
     jquery: 'jQuery',
   },
   plugins: [
-    new CleanPlugin([config.paths.dist], {
-      root: config.paths.root,
+    new ESLintPlugin({
+      failOnWarning: false,
+      failOnError: true,
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [config.paths.dist],
       verbose: false,
     }),
-    /**
-     * It would be nice to switch to copy-webpack-plugin, but
-     * unfortunately it doesn't provide a reliable way of
-     * tracking the before/after file names
-     */
-    new CopyGlobsPlugin({
-      pattern: config.copy,
-      output: `[path]${assetsFilenames}.[ext]`,
-      manifest: config.manifest,
-    }),
-    new ExtractTextPlugin({
-      filename: `styles/${assetsFilenames}.css`,
-      allChunks: true,
-      disable: (config.enabled.watcher),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: config.copy,
+          noErrorOnMissing: true,
+          to: `[path]${assetsFilenames}[ext]`, // Note: since 8.0.0 no dot in placeholder needed
+        },
+      ],
     }),
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
       'window.jQuery': 'jquery',
-      Popper: '@popperjs/core/dist/umd/popper.js',
+      Popper: '@popperjs/dist/umd/popper.js',
     }),
     new webpack.LoaderOptionsPlugin({
       minimize: config.enabled.optimize,
@@ -171,11 +152,8 @@ let webpackConfig = {
         context: config.paths.assets,
       },
     }),
-    new webpack.LoaderOptionsPlugin({
-      test: /\.js$/,
-      options: {
-        eslint: { failOnWarning: false, failOnError: true },
-      },
+    new MiniCssExtractPlugin({
+      filename: `styles/${assetsFilenames}.css`,
     }),
     new StyleLintPlugin({
       failOnError: !config.enabled.watcher,
@@ -191,10 +169,6 @@ if (config.enabled.optimize) {
   webpackConfig = merge(webpackConfig, require('./webpack.config.optimize'));
 }
 
-if (config.env.production) {
-  webpackConfig.plugins.push(new webpack.NoEmitOnErrorsPlugin());
-}
-
 if (config.enabled.cacheBusting) {
   const WebpackAssetsManifest = require('webpack-assets-manifest');
 
@@ -205,6 +179,7 @@ if (config.enabled.cacheBusting) {
       writeToDisk: false,
       assets: config.manifest,
       replacer: require('./util/assetManifestsFormatter'),
+      contextRelativeKeys: true,
     })
   );
 }
@@ -223,6 +198,14 @@ if (config.enabled.watcher) {
  * file, as there are limitations to using webpack-merge which can hinder your
  * ability to change certain options.
  */
-module.exports = merge.smartStrategy({
-  'module.loaders': 'replace',
-})(webpackConfig, desire(`${__dirname}/webpack.config.preset`));
+
+module.exports = mergeWithCustomize({
+  customizeArray: customizeArray({
+    'module.rules': 'replace',
+  }),
+})(
+  webpackConfig,
+  desire(`${__dirname}/webpack.config.preset`)
+    ? desire(`${__dirname}/webpack.config.preset`)
+    : {}
+);
