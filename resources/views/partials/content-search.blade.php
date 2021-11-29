146a169a073f43d92261cf4642a1abd7a80e9baf
sarahst <sarah@stirlingbrandworks.com>
2021-11-29 15:24:36 -0500

    Styling for the search results and page

--- resources/views/partials/content-search.blade.php
+++ resources/views/partials/content-search.blade.php
@@ -1,16 +1,18 @@
 <article @php post_class() @endphp>
-  <header>
+  <header class="pt-2">
     @if( 'database' == get_post_type() )
-		<h2 class="entry-title"><a href="{{ the_field('database_url') }}">{!! get_the_title() !!}</a></h2>
+		<h4 class="entry-title m-0"><a href="{{ the_field('database_url') }}" class="text-dark text-decoration-none">{!! get_the_title() !!}</a> <small class="text-capitalize fw-light text-muted small">| {{ get_post_type() }}</small></h4>
+		<a href="{{ the_field('database_url') }}" class="small text-primary">{{ the_field('database_url') }}</a>
   	@else
-    	<h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
+    	<h4 class="entry-title"><a href="{{ get_permalink() }}" class="text-dark text-decoration-none">{!! get_the_title() !!}</a> <small class="text-capitalize fw-light text-muted small">| {{ get_post_type() }}</small></h4>
   	@endif
 
     @if (get_post_type() === 'post')
       @include('partials/entry-meta')
     @endif
   </header>
-  <div class="entry-summary">
+  <div class="entry-summary pb-2 text-muted">
     @php the_excerpt() @endphp
   </div>
+  <hr>
 </article>
--- resources/views/partials/entry-meta.blade.php
+++ resources/views/partials/entry-meta.blade.php
@@ -1,6 +1,2 @@
-<time class="updated" datetime="{{ get_post_time('c', true) }}">{{ get_the_date() }}</time>
-<p class="byline author vcard">
-  {{ __('By', 'slab') }} <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
-    {{ get_the_author() }}
-  </a>
-</p>
+<time class="small updated d-inline-block text-dark text-uppercase" datetime="{{ get_post_time('c', true) }}">{{ get_the_date('M jS, Y') }}</time>
+ <span class="text-muted">|</span> <p class="small d-inline-block byline author vcard"><a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn text-secondary text-decoration-none">{{ get_the_author() }}</a></p>
