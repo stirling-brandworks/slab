723cea61d4b7f9f7c3ea897571a28c8786255a26
sarahst <sarah@stirlingbrandworks.com>
2021-12-06 13:48:56 -0500

    Added in all blank meta files for the branch meta

--- resources/views/partials/shelf/branches/map.blade.php
+++ resources/views/partials/shelf/branches/map.blade.php
@@ -1,1 +1,3 @@
-<div>Map</div>
+<div class="map-wrapper map-wrapper--raised position-relative z-2 slab-edge border overflow-hidden mb-5">
+	Map
+</div>
--- resources/views/partials/shelf/branches/thumbs.blade.php
+++ resources/views/partials/shelf/branches/thumbs.blade.php
@@ -1,1 +1,24 @@
-<div>Thumb List of Branches</div>
+<div>
+	@php
+		$args = array('post_type' => 'branch', 'numberposts' => 20);
+		$branches = get_posts( $args )
+	@endphp
+
+	@if ( $branches )
+	<div class="cards-wrapper">
+		@foreach ($branches as $branch)
+			<div class="mt-5">
+				@include('components.card', [
+				    'title' => $branch->post_title,
+				    'date' => null,
+				    'content' => null,
+				    'link' => ['title'=>'Branch Details', 'url'=> get_post_permalink($branch) ],
+				    'image' => get_post_thumbnail_id($branch),
+				    'layout' => 'vertical',
+				    'imageSize' => 'large',
+				])
+			</div>
+		@endforeach
+	</div>
+	@endif
+</div>
