d53a258931f5b1d014b03290d1f3dfcf0dda4ca0
sarahst <sarah@stirlingbrandworks.com>
2021-12-07 15:48:54 -0500

    Replaced the card compoennt with database component for subject items on DB template

--- resources/views/partials/shelf/databases/databases-subject.blade.php
+++ resources/views/partials/shelf/databases/databases-subject.blade.php
@@ -3,11 +3,12 @@
 @foreach ($items_by_subject as $subject => $databases)
   <h2 class="fw-bold" id="{!! $subject !!}">{!! $subject !!}</h2>
 
-  <ul class="list-unstyled card-list  p-0">
+  <div class="databases-wrapper">
     @foreach ($databases as $database)
-      <li class="w-100 p-0">
-        @include('components.card', $database)
-      </li>
-    @endforeach
-  </ul>
+	  @include('components.shelf.database-item', [
+		    'image' => get_post_thumbnail_id(),
+		    'layout' => 'horizontal',
+		], $database)
+  	@endforeach
+  </div>
 @endforeach
