4326c3c51a4717d72df2ac78de5e34d59c74f972
sarahst <sarah@stirlingbrandworks.com>
2021-12-07 12:45:36 -0500

    Added back to top button markup and styles

--- resources/views/partials/back-to-top.php
+++ resources/views/partials/back-to-top.php
@@ -1,1 +1,8 @@
-<a href="#top">Return to Top</a>
+<a href="#top" class="rtt-button transition text-center d-inline-block text-decoration-none z-5">
+	<div class="rtt-button__icon mx-auto shadow-sm bg-contrast mb-2">
+		<svg xmlns="http://www.w3.org/2000/svg" width="17.999" height="18" viewBox="0 0 17.999 18">
+		  <path id="white-arrow-top" d="M16.1,24.042l1.9,1.9,9-9-9-9-1.9,1.9,5.76,5.76H9v2.684H21.863Z" transform="translate(-7.94 27) rotate(-90)" fill="#fff"/>
+		</svg>
+	</div>
+	<p class="m-0 small text-uppercase text-dark fw-bold lh-1">Return<br>to Top</p>
+</a>
