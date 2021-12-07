b307bcdaf6a8e5f62322007178044791d8d9a0cd
sarahst <sarah@stirlingbrandworks.com>
2021-12-07 16:23:45 -0500

    Added icons to legend and database items for special detials

--- resources/views/partials/shelf/databases/database-meta.blade.php
+++ resources/views/partials/shelf/databases/database-meta.blade.php
@@ -1,4 +1,5 @@
 <h4 class="m-0">
 	<a href="{{ $link['url'] }}" class="stretched-link text-decoration-none text-dark">{!! $title !!}</a> 
-	@if($cardRequired) Card Required @endif @if($inLibraryOnly) In-Library @endif
+	@if($cardRequired) <img src="@asset('images/icons/library-card.svg')" alt="Library Card Icon" height="20" class="ms-2 position-relative z-2"  title="Library Card Required"/> @endif 
+	@if($inLibraryOnly) <img src="@asset('images/icons/library-building.svg')" alt="Library Building Icon" height="22" class="ms-2 position-relative z-2" title="In-Library Use Only"/> @endif
 </h4>
