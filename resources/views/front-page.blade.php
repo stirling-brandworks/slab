dcc0a2498cc2bd960b32fb93020e3060a6fb7a8c
Brian John Hanna <bjhanna728@gmail.com>
2022-03-11 15:05:21 -0500

    Update page-header template part placement

--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.full-width')
 
 @section('content')
-@while(have_posts()) @php the_post() @endphp
-@include('partials.page-header')
-@include('partials.content-page')
-@endwhile
+    @include('partials.page-header')
+    @while (have_posts())
+        @php the_post() @endphp
+        @include('partials.content-page')
+    @endwhile
 @endsection
